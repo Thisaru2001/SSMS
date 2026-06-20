5f7a5464f27536491cfe2bfe1ae773d10ddf2d50
uthpalacb <sandeepanicbu@gmail.com>
2026-06-20 17:48:48 +0530

    student detail update created

--- SSMS/Program.cs
+++ SSMS/Program.cs
@@ -11,10 +11,10 @@ namespace SSMS
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Principal_Dashbaord());
+            Application.Run(new StudentDetail());
 
 
-           
+       
 
 
         }
---
+++ SSMS/StudentDetail.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace SSMS
+{
+    partial class StudentDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            txtID = new TextBox();
+            txtStudentName = new TextBox();
+            txtAddress = new TextBox();
+            txtGuardians = new TextBox();
+            txtPhone = new TextBox();
+            txtGrade = new TextBox();
+            btnSearch = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1150, 96);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(35, 10);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(99, 83);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Green;
+            label1.Location = new Point(378, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(333, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Student Detail Update";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(139, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(128, 31);
+            label2.TabIndex = 1;
+            label2.Text = "StudentID:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(134, 222);
+            label3.Name = "label3";
+            label3.Size = new Size(173, 31);
+            label3.TabIndex = 2;
+            label3.Text = "Student Name:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(134, 290);
+            label4.Name = "label4";
+            label4.Size = new Size(105, 31);
+            label4.TabIndex = 3;
+            label4.Text = "Address:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(134, 357);
+            label5.Name = "label5";
+            label5.Size = new Size(128, 31);
+            label5.TabIndex = 4;
+            label5.Text = "Guardians:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(134, 425);
+            label6.Name = "label6";
+            label6.Size = new Size(88, 31);
+            label6.TabIndex = 5;
+            label6.Text = "Phone:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(134, 496);
+            label7.Name = "label7";
+            label7.Size = new Size(83, 31);
+            label7.TabIndex = 6;
+            label7.Text = "Grade:";
+            // 
+            // txtID
+            // 
+            txtID.Location = new Point(355, 164);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(393, 27);
+            txtID.TabIndex = 7;
+            // 
+            // txtStudentName
+            // 
+            txtStudentName.Location = new Point(359, 229);
+            txtStudentName.Name = "txtStudentName";
+            txtStudentName.Size = new Size(389, 27);
+            txtStudentName.TabIndex = 8;
+            // 
+            // txtAddress
+            // 
+            txtAddress.Location = new Point(357, 294);
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(391, 27);
+            txtAddress.TabIndex = 9;
+            // 
+            // txtGuardians
+            // 
+            txtGuardians.Location = new Point(357, 366);
+            txtGuardians.Name = "txtGuardians";
+            txtGuardians.Size = new Size(391, 27);
+            txtGuardians.TabIndex = 10;
+            // 
+            // txtPhone
+            // 
+            txtPhone.Location = new Point(355, 434);
+            txtPhone.Name = "txtPhone";
+            txtPhone.Size = new Size(393, 27);
+            txtPhone.TabIndex = 11;
+            // 
+            // txtGrade
+            // 
+            txtGrade.Location = new Point(359, 493);
+            txtGrade.Name = "txtGrade";
+            txtGrade.Size = new Size(389, 27);
+            txtGrade.TabIndex = 12;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSearch.ForeColor = Color.Green;
+            btnSearch.Location = new Point(865, 164);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(123, 43);
+            btnSearch.TabIndex = 13;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Green;
+            button1.Location = new Point(378, 609);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 41);
+            button1.TabIndex = 14;
+            button1.Text = "Update";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.Green;
+            button2.Location = new Point(576, 607);
+            button2.Name = "button2";
+            button2.Size = new Size(135, 43);
+            button2.TabIndex = 15;
+            button2.Text = "Clear";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // StudentDetail
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1150, 708);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(btnSearch);
+            Controls.Add(txtGrade);
+            Controls.Add(txtPhone);
+            Controls.Add(txtGuardians);
+            Controls.Add(txtAddress);
+            Controls.Add(txtStudentName);
+            Controls.Add(txtID);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "StudentDetail";
+            Text = "StudentDetail";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox txtID;
+        private TextBox txtStudentName;
+        private TextBox txtAddress;
+        private TextBox txtGuardians;
+        private TextBox txtPhone;
+        private TextBox txtGrade;
+        private PictureBox pictureBox1;
+        private Button btnSearch;
+        private Button button1;
+        private Button button2;
+    }
+}
