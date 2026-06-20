41aa441592d6f22c2ec3fbdf31885475a06d8a50
Gimhani06 <gimhani1206@gmail.com>
2026-06-20 18:20:11 +0530

    create notice panel for principal

---
+++ SSMS/notice_create.Designer.cs
@@ -0,0 +1,397 @@
+﻿namespace SSMS
+{
+    partial class notice_create
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            pictureBox1 = new PictureBox();
+            btnTeacherAttendance = new Button();
+            btnLogout = new Button();
+            btnTimetable = new Button();
+            btnProfile = new Button();
+            btnStudentRegister = new Button();
+            btnNoticeCreate = new Button();
+            btnTeacherRegister = new Button();
+            adminContentPanel = new Panel();
+            button1 = new Button();
+            rtbNoticeContent = new RichTextBox();
+            txtNoticeTitle = new TextBox();
+            label4 = new Label();
+            pnlGroupSelection = new Panel();
+            chkstudent = new CheckBox();
+            chkTeachers = new CheckBox();
+            chkall = new CheckBox();
+            label3 = new Label();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            adminContentPanel.SuspendLayout();
+            pnlGroupSelection.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(pictureBox1);
+            splitContainer1.Panel1.Controls.Add(btnTeacherAttendance);
+            splitContainer1.Panel1.Controls.Add(btnLogout);
+            splitContainer1.Panel1.Controls.Add(btnTimetable);
+            splitContainer1.Panel1.Controls.Add(btnProfile);
+            splitContainer1.Panel1.Controls.Add(btnStudentRegister);
+            splitContainer1.Panel1.Controls.Add(btnNoticeCreate);
+            splitContainer1.Panel1.Controls.Add(btnTeacherRegister);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(adminContentPanel);
+            splitContainer1.Panel2.Controls.Add(pictureBox2);
+            splitContainer1.Panel2.Controls.Add(label2);
+            splitContainer1.Panel2.Controls.Add(label1);
+            splitContainer1.Size = new Size(1150, 708);
+            splitContainer1.SplitterDistance = 280;
+            splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.ImeMode = ImeMode.NoControl;
+            pictureBox1.Location = new Point(90, 25);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 100);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 18;
+            pictureBox1.TabStop = false;
+            // 
+            // btnTeacherAttendance
+            // 
+            btnTeacherAttendance.BackColor = Color.White;
+            btnTeacherAttendance.FlatAppearance.BorderSize = 0;
+            btnTeacherAttendance.FlatStyle = FlatStyle.Flat;
+            btnTeacherAttendance.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            btnTeacherAttendance.ImeMode = ImeMode.NoControl;
+            btnTeacherAttendance.Location = new Point(0, 321);
+            btnTeacherAttendance.Name = "btnTeacherAttendance";
+            btnTeacherAttendance.Size = new Size(280, 45);
+            btnTeacherAttendance.TabIndex = 25;
+            btnTeacherAttendance.Text = "Teacher Attendence";
+            btnTeacherAttendance.TextAlign = ContentAlignment.MiddleLeft;
+            btnTeacherAttendance.UseVisualStyleBackColor = false;
+            // 
+            // btnLogout
+            // 
+            btnLogout.BackColor = Color.White;
+            btnLogout.FlatAppearance.BorderSize = 0;
+            btnLogout.FlatStyle = FlatStyle.Flat;
+            btnLogout.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            btnLogout.ImeMode = ImeMode.NoControl;
+            btnLogout.Location = new Point(0, 456);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(280, 45);
+            btnLogout.TabIndex = 28;
+            btnLogout.Text = "Logout";
+            btnLogout.TextAlign = ContentAlignment.MiddleLeft;
+            btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // btnTimetable
+            // 
+            btnTimetable.BackColor = Color.White;
+            btnTimetable.FlatAppearance.BorderSize = 0;
+            btnTimetable.FlatStyle = FlatStyle.Flat;
+            btnTimetable.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            btnTimetable.ImeMode = ImeMode.NoControl;
+            btnTimetable.Location = new Point(0, 276);
+            btnTimetable.Name = "btnTimetable";
+            btnTimetable.Size = new Size(280, 45);
+            btnTimetable.TabIndex = 24;
+            btnTimetable.Text = "Timetable";
+            btnTimetable.TextAlign = ContentAlignment.MiddleLeft;
+            btnTimetable.UseVisualStyleBackColor = false;
+            // 
+            // btnProfile
+            // 
+            btnProfile.BackColor = Color.White;
+            btnProfile.FlatAppearance.BorderSize = 0;
+            btnProfile.FlatStyle = FlatStyle.Flat;
+            btnProfile.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            btnProfile.ImeMode = ImeMode.NoControl;
+            btnProfile.Location = new Point(0, 366);
+            btnProfile.Name = "btnProfile";
+            btnProfile.Size = new Size(280, 45);
+            btnProfile.TabIndex = 26;
+            btnProfile.Text = "Profile";
+            btnProfile.TextAlign = ContentAlignment.MiddleLeft;
+            btnProfile.UseVisualStyleBackColor = false;
+            // 
+            // btnStudentRegister
+            // 
+            btnStudentRegister.BackColor = Color.White;
+            btnStudentRegister.FlatAppearance.BorderSize = 0;
+            btnStudentRegister.FlatStyle = FlatStyle.Flat;
+            btnStudentRegister.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            btnStudentRegister.ForeColor = Color.Black;
+            btnStudentRegister.ImageAlign = ContentAlignment.MiddleLeft;
+            btnStudentRegister.ImeMode = ImeMode.NoControl;
+            btnStudentRegister.Location = new Point(0, 231);
+            btnStudentRegister.Name = "btnStudentRegister";
+            btnStudentRegister.Size = new Size(280, 45);
+            btnStudentRegister.TabIndex = 22;
+            btnStudentRegister.Text = "Student Register";
+            btnStudentRegister.TextAlign = ContentAlignment.MiddleLeft;
+            btnStudentRegister.UseVisualStyleBackColor = false;
+            // 
+            // btnNoticeCreate
+            // 
+            btnNoticeCreate.BackColor = Color.FromArgb(225, 245, 230);
+            btnNoticeCreate.FlatAppearance.BorderSize = 0;
+            btnNoticeCreate.FlatStyle = FlatStyle.Flat;
+            btnNoticeCreate.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            btnNoticeCreate.ForeColor = Color.FromArgb(0, 120, 70);
+            btnNoticeCreate.ImeMode = ImeMode.NoControl;
+            btnNoticeCreate.Location = new Point(0, 411);
+            btnNoticeCreate.Name = "btnNoticeCreate";
+            btnNoticeCreate.Size = new Size(280, 45);
+            btnNoticeCreate.TabIndex = 27;
+            btnNoticeCreate.Text = "Notice Create";
+            btnNoticeCreate.TextAlign = ContentAlignment.MiddleLeft;
+            btnNoticeCreate.UseVisualStyleBackColor = false;
+            // 
+            // btnTeacherRegister
+            // 
+            btnTeacherRegister.BackColor = Color.White;
+            btnTeacherRegister.FlatAppearance.BorderSize = 0;
+            btnTeacherRegister.FlatStyle = FlatStyle.Flat;
+            btnTeacherRegister.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            btnTeacherRegister.ForeColor = Color.Black;
+            btnTeacherRegister.ImageAlign = ContentAlignment.BottomLeft;
+            btnTeacherRegister.ImeMode = ImeMode.NoControl;
+            btnTeacherRegister.Location = new Point(0, 186);
+            btnTeacherRegister.Name = "btnTeacherRegister";
+            btnTeacherRegister.Size = new Size(280, 45);
+            btnTeacherRegister.TabIndex = 20;
+            btnTeacherRegister.Text = "Teacher Register";
+            btnTeacherRegister.TextAlign = ContentAlignment.MiddleLeft;
+            btnTeacherRegister.UseVisualStyleBackColor = false;
+            // 
+            // adminContentPanel
+            // 
+            adminContentPanel.BackColor = Color.White;
+            adminContentPanel.Controls.Add(button1);
+            adminContentPanel.Controls.Add(rtbNoticeContent);
+            adminContentPanel.Controls.Add(txtNoticeTitle);
+            adminContentPanel.Controls.Add(label4);
+            adminContentPanel.Controls.Add(pnlGroupSelection);
+            adminContentPanel.Controls.Add(label3);
+            adminContentPanel.Location = new Point(40, 120);
+            adminContentPanel.Name = "adminContentPanel";
+            adminContentPanel.Size = new Size(800, 560);
+            adminContentPanel.TabIndex = 24;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(0, 135, 80);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(560, 485);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 48);
+            button1.TabIndex = 5;
+            button1.Text = "Publish Notice";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // rtbNoticeContent
+            // 
+            rtbNoticeContent.BorderStyle = BorderStyle.FixedSingle;
+            rtbNoticeContent.Location = new Point(40, 215);
+            rtbNoticeContent.Name = "rtbNoticeContent";
+            rtbNoticeContent.Size = new Size(720, 220);
+            rtbNoticeContent.TabIndex = 4;
+            rtbNoticeContent.Text = "";
+            // 
+            // txtNoticeTitle
+            // 
+            txtNoticeTitle.Location = new Point(40, 155);
+            txtNoticeTitle.Name = "txtNoticeTitle";
+            txtNoticeTitle.Size = new Size(720, 27);
+            txtNoticeTitle.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(40, 125);
+            label4.Name = "label4";
+            label4.Size = new Size(182, 25);
+            label4.TabIndex = 2;
+            label4.Text = "Notice Title/Subject";
+            // 
+            // pnlGroupSelection
+            // 
+            pnlGroupSelection.BackColor = Color.FromArgb(195, 240, 210);
+            pnlGroupSelection.Controls.Add(chkstudent);
+            pnlGroupSelection.Controls.Add(chkTeachers);
+            pnlGroupSelection.Controls.Add(chkall);
+            pnlGroupSelection.Location = new Point(40, 60);
+            pnlGroupSelection.Name = "pnlGroupSelection";
+            pnlGroupSelection.Size = new Size(720, 45);
+            pnlGroupSelection.TabIndex = 1;
+            // 
+            // chkstudent
+            // 
+            chkstudent.AutoSize = true;
+            chkstudent.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkstudent.Location = new Point(440, 12);
+            chkstudent.Name = "chkstudent";
+            chkstudent.Size = new Size(103, 27);
+            chkstudent.TabIndex = 2;
+            chkstudent.Text = "Students";
+            chkstudent.UseVisualStyleBackColor = true;
+            // 
+            // chkTeachers
+            // 
+            chkTeachers.AutoSize = true;
+            chkTeachers.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkTeachers.Location = new Point(220, 12);
+            chkTeachers.Name = "chkTeachers";
+            chkTeachers.Size = new Size(99, 27);
+            chkTeachers.TabIndex = 1;
+            chkTeachers.Text = "Teachers";
+            chkTeachers.UseVisualStyleBackColor = true;
+            // 
+            // chkall
+            // 
+            chkall.AutoSize = true;
+            chkall.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkall.Location = new Point(25, 12);
+            chkall.Name = "chkall";
+            chkall.Size = new Size(54, 27);
+            chkall.TabIndex = 0;
+            chkall.Text = "All";
+            chkall.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(40, 30);
+            label3.Name = "label3";
+            label3.Size = new Size(65, 25);
+            label3.TabIndex = 0;
+            label3.Text = "Group";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(809, 139);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(45, 45);
+            pictureBox2.TabIndex = 23;
+            pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ImeMode = ImeMode.NoControl;
+            label2.Location = new Point(40, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(251, 23);
+            label2.TabIndex = 21;
+            label2.Text = "Welcome, Mr. John (Principal)";
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(0, 120, 70);
+            label1.ImeMode = ImeMode.NoControl;
+            label1.Location = new Point(40, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(454, 31);
+            label1.TabIndex = 19;
+            label1.Text = "SMART SCHOOL MANAGEMENT SYSTEM";
+            // 
+            // notice_create
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1150, 708);
+            Controls.Add(splitContainer1);
+            Name = "notice_create";
+            Text = "notice_create";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            adminContentPanel.ResumeLayout(false);
+            adminContentPanel.PerformLayout();
+            pnlGroupSelection.ResumeLayout(false);
+            pnlGroupSelection.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private PictureBox pictureBox1;
+        private Button btnLogout;
+        private Button btnProfile;
+        private Button btnNoticeCreate;
+        private Button btnTeacherRegister;
+        private Button btnStudentRegister;
+        private Button btnTeacherAttendance;
+        private Button btnTimetable;
+        private PictureBox pictureBox2;
+        private Label label2;
+        private Label label1;
+        private Panel adminContentPanel;
+        private Panel pnlGroupSelection;
+        private CheckBox chkTeachers;
+        private CheckBox chkall;
+        private Label label3;
+        private TextBox txtNoticeTitle;
+        private Label label4;
+        private CheckBox chkstudent;
+        private RichTextBox rtbNoticeContent;
+        private Button button1;
+    }
+}
