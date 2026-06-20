28784da77af5a470f3c2adccf5ac5d96cbe560c9
uthpalacb <sandeepanicbu@gmail.com>
2026-06-20 20:00:51 +0530

    create notice

--- SSMS/Program.cs
+++ SSMS/Program.cs
@@ -11,7 +11,7 @@ namespace SSMS
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new teacher_register());
+            Application.Run(new CreateNotice());
 
 
            
